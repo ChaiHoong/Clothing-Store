08862e9ddf2088f8eb98576ff30085291fa81f4a
Chai Hoong <chaihoong02@gmail.com>
2025-04-06 09:22:24 +0800

    w

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: trfalseue,
+    production: false,
     rapidApiKey: '5400c35a5dmshc2c299d11e9143ep15cabcjsn9b876ec7f903',
     rapidApiHost: 'kohls.p.rapidapi.com'
   };
